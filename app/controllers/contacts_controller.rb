3765d0a36bebd8b36da76c95199f9750680e7e4e
mikitakezawa <mikiaaamiki@yahoo.co.jp>
2023-05-15 19:38:18 +0900

    create controller new create

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,14 @@
 class ContactsController < ApplicationController
   def new
+      @contact = Contact.new(contact_params)
   end
+  
+  def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+          redirect_to new_contact_path
+      else
+          render :new
+      end
+   end
 end
